28de21b9a26e469b4632922204599d0577741a29
Cha Yang <chachaiyang3@gmail.com>
2019-09-24 00:14:54 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,9 +6,10 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    
-    if BRANDS.include?(brand) == nil
-      BRANDS << brand
+    if BRANDS.include?(brand)
+      @brand
+    else
+      BRANDS << @brand
     end
   end
 
